74bdc396abc6904b94aa9790c0ff931aee6db308
albertsendrosjimenez <sendros@pm.me>
2020-03-15 18:14:23 +0000

    Bug fix: There was missing curly braces around reducer import preventing the compilation of the project.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import reducer from './store/reducer'
+import {reducer} from './store/reducer'
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(reducer)
+const store = createStore(reducer);
 
 ReactDOM.render(<Provider store={store}><App /> </Provider>, document.getElementById('root'));
 
